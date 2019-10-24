704b0c9528d816ccbc08cb0e50e5a75702f2dbef
Alfred Bach <abach@redhat.com>
2019-10-24 13:14:21 +0200

    agasin

---
+++ js/milano-2.20191024131054.js
@@ -0,0 +1,18 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js5').attr('src', (dpi>1) ? 'images/eingesetztes-bild-202.png' : 'images/eingesetztes-bild-101.png');
+$('.js6').attr('src', (dpi>1) ? 'images/eingesetztes-bild-372.png' : 'images/eingesetztes-bild-186.png');};
+if(!window.HTMLPictureElement){r();}
+if(! document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")) {
+$('.js7').attr('src', 'images/fb-47.png');
+$('.js8').attr('src', 'images/fb-59.png');
+
+}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+initMenu($('#m1')[0]);
+$('.c14').Stickyfill();
+
+});
