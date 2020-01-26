d44e685abc881be46312087643e77879befc6e08
Alfred Bach <abach@redhat.com>
2020-01-26 20:29:34 +0100

    nnewone

--- js/index.20200126202924.js
+++ js/index.20200126202924.js
@@ -4,8 +4,8 @@ $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof att
 
 $(document).ready(function() {
 r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/ibm_logo-r-_redhat_lockup_horiz_rev_color_rgb-592.png' : 'images/ibm_logo-r-_redhat_lockup_horiz_rev_color_rgb-296.png');
-$('.js2').attr('src', (dpi>1) ? 'images/eingesetztes-bild-1528.jpg' : 'images/eingesetztes-bild-764.jpg');
-$('.js3').attr('src', (dpi>1) ? 'images/eingesetztes-bild-372.png' : 'images/eingesetztes-bild-186.png');};
+$('.js2').attr('src', (dpi>1) ? 'images/eingesetztes-bild-372.png' : 'images/eingesetztes-bild-186.png');
+$('.js3').attr('src', (dpi>1) ? 'images/eingesetztes-bild-1352.jpg' : 'images/eingesetztes-bild-676.jpg');};
 if(!window.HTMLPictureElement){r();}
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 initMenu($('#m1')[0]);
